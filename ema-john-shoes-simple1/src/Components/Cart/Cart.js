06482d3dc7a0ce38adcdd9fb48d6a38e2c7bd89d
Salman-S-Rahman <salmanmern@gmail.com>
2022-06-13 00:13:40 +0600

    total cost find out

--- ema-john-shoes-simple1/src/Components/Cart/Cart.js
+++ ema-john-shoes-simple1/src/Components/Cart/Cart.js
@@ -2,17 +2,26 @@ import React from "react";
 import "./Cart.css";
 
 const Cart = ({ cart }) => {
-  console.log(cart);
   let subPrice = 0;
-  for (const price in cart) {
-    subPrice = price + subPrice;
-    console.log(subPrice);
+  let shipping = 0;
+  for (const product of cart) {
+    console.log(product);
+    subPrice = product.price + subPrice;
+    shipping = product.shipping + shipping;
   }
+  const tax = subPrice * 0.1;
+  const grandTotal = subPrice + shipping + tax;
 
   return (
-    <div className="cart-container p-5">
-      <h3>Order Summary:</h3>
+    <div className="cart-container py-5 ps-2">
+      <h2>Order Summary:</h2>
       <h4>Selected Items: {cart.length}</h4>
+      <h4>Total Price:${subPrice.toFixed(2)}</h4>
+      <h4>Shipping:${shipping.toFixed(2)}</h4>
+      <h4>Tax:${tax.toFixed(2)}</h4>
+      <h4 className="fw-bold text-danger">
+        Grand Total :${grandTotal.toFixed(2)}
+      </h4>
     </div>
   );
 };
