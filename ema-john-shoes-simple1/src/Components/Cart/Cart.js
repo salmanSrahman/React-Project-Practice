733edd1b4a1743da2fcb0caae089a7b433c066b3
Salman-S-Rahman <salmanmern@gmail.com>
2022-06-14 01:53:14 +0600

    data showed in display from local storage

--- ema-john-shoes-simple1/src/Components/Cart/Cart.js
+++ ema-john-shoes-simple1/src/Components/Cart/Cart.js
@@ -2,11 +2,14 @@ import React from "react";
 import "./Cart.css";
 
 const Cart = ({ cart }) => {
+  console.log(cart);
   let subPrice = 0;
   let shipping = 0;
+  let quantity = 0;
   for (const product of cart) {
-    subPrice = product.price + subPrice;
-    shipping = product.shipping + shipping;
+    quantity = quantity + product.quantity;
+    subPrice = product.price * product.quantity + subPrice;
+    shipping = product.shipping * product.quantity + shipping;
   }
   const tax = subPrice * 0.1;
   const grandTotal = subPrice + shipping + tax;
@@ -14,7 +17,7 @@ const Cart = ({ cart }) => {
   return (
     <div className="cart-container py-5 ps-2">
       <h2>Order Summary:</h2>
-      <h4>Selected Items: {cart.length}</h4>
+      <h4>Selected Items: {quantity}</h4>
       <h4>Total Price:${subPrice.toFixed(2)}</h4>
       <h4>Shipping:${shipping.toFixed(2)}</h4>
       <h4>Tax:${tax.toFixed(2)}</h4>
--- ema-john-shoes-simple1/src/Shop/Shop.js
+++ ema-john-shoes-simple1/src/Shop/Shop.js
@@ -14,15 +14,23 @@ const Shop = () => {
       .then((data) => setProducts(data));
   }, []);
 
-  const addToCart = (product) => {
-    const newCart = [...cart, product];
+  const addToCart = (selectedProduct) => {
+    let newCart = [];
+    const exists = cart.find((product) => product.id === selectedProduct.id);
+    if (!exists) {
+      selectedProduct.quantity = 1;
+      newCart = [...cart, selectedProduct];
+    } else {
+      const rest = cart.filter((product) => product.id !== selectedProduct.id);
+      exists.quantity = exists.quantity + 1;
+      newCart = [...rest, exists];
+    }
     setCart(newCart);
-    addToDb(product.id);
+    addToDb(selectedProduct.id);
   };
 
   useEffect(() => {
     const storedCart = getStoredCart();
-
     const savedCart = [];
     for (const id in storedCart) {
       const addedProduct = products.find((product) => product.id === id);
