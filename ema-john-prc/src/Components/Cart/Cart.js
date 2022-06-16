7e26512875e9e380c5d188069916970b7efe8451
Salman-S-Rahman <salmanmern@gmail.com>
2022-06-16 23:13:00 +0600

    total price calculeted

--- ema-john-prc/src/Components/Cart/Cart.js
+++ ema-john-prc/src/Components/Cart/Cart.js
@@ -2,10 +2,23 @@ import React from "react";
 import style from "./Cart.module.css";
 
 const Cart = ({ cart }) => {
+  console.log(cart);
+  let subPrice = 0;
+  let shipping = 0;
+  for (const product of cart) {
+    subPrice = subPrice + product.price;
+    shipping = shipping + product.shipping;
+  }
+  let tax = subPrice * 0.1;
+  const grandTotal = subPrice + shipping + tax;
   return (
     <div className={`${style.cart__container}`}>
       <h3>Order Summary</h3>
       <h5>Selected Items: {cart.length}</h5>
+      <h5>Total Price: ${subPrice}</h5>
+      <h5>Shipping: ${shipping}</h5>
+      <h5>Tax: ${tax}</h5>
+      <h4>Grand Total: ${grandTotal}</h4>
     </div>
   );
 };
