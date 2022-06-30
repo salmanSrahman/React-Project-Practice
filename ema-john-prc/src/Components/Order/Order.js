ae538f46f9ed69b8fae39604362a82b0a8cb4d32
Salman-S-Rahman <salmanmern@gmail.com>
2022-06-30 12:01:15 +0600

    cart done

--- ema-john-prc/src/Components/Order/Order.js
+++ ema-john-prc/src/Components/Order/Order.js
@@ -3,7 +3,7 @@ import { Col, Container, Row } from "react-bootstrap";
 import useCart from "../../Hooks/useCart";
 import useProducts from "../../Hooks/useProducts";
 import Cart from "../Cart/Cart";
-import ReviewItem from "../ReviewItem/ReviewItem";
+// import ReviewItem from "../ReviewItem/ReviewItem";
 
 const Order = () => {
   const [products] = useProducts();
@@ -14,7 +14,11 @@ const Order = () => {
     <div>
       <Container>
         <Row className="g-3">
-          <Col md={9}></Col>
+          <Col md={9}>
+            {/* {cart.map((product) => (
+              <ReviewItem product={product}></ReviewItem>
+            ))} */}
+          </Col>
           <Col md={3}>
             <Cart cart={cart}></Cart>
           </Col>
