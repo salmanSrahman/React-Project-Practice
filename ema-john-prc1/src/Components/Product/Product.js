c94edf669adb83fbf1251d1783cdb6adeab2b31d
Salman-S-Rahman <salmanmern@gmail.com>
2022-07-02 11:46:21 +0600

    ratings added

--- ema-john-prc1/src/Components/Product/Product.js
+++ ema-john-prc1/src/Components/Product/Product.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Button, Col } from "react-bootstrap";
+import Rating from "react-rating";
 import "./Product.css";
 
 const Product = ({ product, addToCart }) => {
-  const { img, name, price, seller, stock } = product;
+  const { img, name, price, seller, stock, ratings } = product;
+  console.log(product);
   return (
     <Col>
       <div className="Product__details">
@@ -15,7 +17,18 @@ const Product = ({ product, addToCart }) => {
           Only <span className="text-danger fw-bold">{stock}</span> left in
           stock - order soon
         </h5>
-        <Button className="d-block w-100 my-1" onClick={()=>addToCart(product)}>Add To Cart</Button>
+        <Rating className="product__ratings"
+          initialRating={ratings}
+          emptySymbol="fa fa-star-o fa-2x"
+          fullSymbol="fa fa-star fa-2x"
+          readonly
+        />
+        <Button
+          className="d-block w-100 my-1"
+          onClick={() => addToCart(product)}
+        >
+          Add To Cart
+        </Button>
       </div>
     </Col>
   );
