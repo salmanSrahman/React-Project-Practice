ff1ee7581967bb6eb8273363c1b244761d954db8
Salman-S-Rahman <salmanmern@gmail.com>
2022-07-02 15:19:47 +0600

    react icon used

--- ema-john-prc1/src/Components/ReviewItem/ReviewItem.js
+++ ema-john-prc1/src/Components/ReviewItem/ReviewItem.js
@@ -1,10 +1,28 @@
 import React from "react";
 import "./ReviewItem.css";
 
-const ReviewItem = (props) => {
+import { RiDeleteBin5Line } from "react-icons/ri";
+
+const ReviewItem = ({ product }) => {
   return (
-    <div>
-      <h5>{props?.product?.name}</h5>
+    <div className=" review__container">
+      <div className="d-flex justify-content-between">
+        <div>
+          <img src={product?.img} alt="" />
+        </div>
+        <div className="d-flex justify-content-center">
+          <div>
+            <h5>{product?.name.slice(0, 25)}</h5>
+            <h5>Price: {product?.price}</h5>
+            <h5>Shipping Charge: {product?.shipping}</h5>
+          </div>
+        </div>
+        <div className="d-flex justify-content-end">
+          <div>
+            <RiDeleteBin5Line />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
