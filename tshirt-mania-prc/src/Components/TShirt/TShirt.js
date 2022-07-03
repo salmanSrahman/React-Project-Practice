676ce08c038cd80b4236b7c7158781b748eb7aab
Salman-S-Rahman <salmanmern@gmail.com>
2022-07-03 20:09:51 +0600

    all components created

---
+++ tshirt-mania-prc/src/Components/TShirt/TShirt.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TShirt.css'
+
+const TShirt = () => {
+      return (
+            <div>
+                  
+            </div>
+      );
+};
+
+export default TShirt;
---
+++ tshirt-mania-prc/src/Components/TShirts/TShirts.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TShirts.css'
+
+const TShirts = () => {
+      return (
+            <div>
+                  
+            </div>
+      );
+};
+
+export default TShirts;
