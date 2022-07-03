23f57ea2d34713fe60bb1068a7cab669a294ce41
Salman-S-Rahman <salmanmern@gmail.com>
2022-07-03 20:32:43 +0600

    fetch data done

--- tshirt-mania-prc/src/Components/Shop/Shop.js
+++ tshirt-mania-prc/src/Components/Shop/Shop.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import './Shop.css'
+import React, { useEffect, useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import "./Shop.css";
+import TShirt from "../TShirt/TShirt";
 
 const Shop = () => {
-      return (
-            <div>
-                  
-            </div>
-      );
+  const [tShirts, setTShirts] = useState([]);
+  useEffect(() => {
+    fetch("tshirts.json")
+      .then((res) => res.json())
+      .then((data) => setTShirts(data));
+  });
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Col>
+            <Row>
+              {tShirts.map((tShirt) => (
+                <TShirt key={tShirt._id} tShirt={tShirt}></TShirt>
+              ))}
+            </Row>
+          </Col>
+          <Col></Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default Shop;
