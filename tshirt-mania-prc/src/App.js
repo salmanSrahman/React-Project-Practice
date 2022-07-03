c56b9a43d49e527173807ad7769f3967f13c75c9
Salman-S-Rahman <salmanmern@gmail.com>
2022-07-03 16:43:55 +0600

    T shirt mania ecommerce

---
+++ tshirt-mania-prc/src/App.js
@@ -0,0 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <div>
+      <h1>T Shirt Mania</h1>
+      {/* <Routes>
+        <Router></Router>
+      </Routes> */}
+    </div>
+  );
+}
+
+export default App;
