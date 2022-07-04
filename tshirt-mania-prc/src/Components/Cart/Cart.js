f99620efbd1d41abd7654908a98e481385ec84c6
Salman-S-Rahman <salmanmern@gmail.com>
2022-07-04 09:48:00 +0600

    conditional rendering button added

--- tshirt-mania-prc/src/Components/Cart/Cart.js
+++ tshirt-mania-prc/src/Components/Cart/Cart.js
@@ -1,14 +1,20 @@
 import React from "react";
 import "./Cart.css";
 import { ImCross } from "react-icons/im";
-
+import { Button } from "react-bootstrap";
 
 const Cart = ({ cart, removeFromCart }) => {
   return (
     <div>
-      <div>
-        <h4>T-Shirt Selected:{cart.length}</h4>
-      </div>
+      {cart.length > 0 ? (
+        <div>
+          <h4>T-Shirt Selected:{cart.length}</h4>
+        </div>
+      ) : (
+        <div>
+          <h4>Please Add T- Shirt:</h4>
+        </div>
+      )}
       <div>
         {cart.map((tShirt) => (
           <div className="w-100 d-flex justify-content-between text-center cart__tShirt">
@@ -22,6 +28,13 @@ const Cart = ({ cart, removeFromCart }) => {
             </span>
           </div>
         ))}
+        {cart.length > 0 ? (
+          <Button className="d-block w-100" variant="success">
+            Proceed Check Out
+          </Button>
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
