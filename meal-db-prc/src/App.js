6197245d184063c8e75ae28fa34b3a4a9a88494b
Salman-S-Rahman <salmanmern@gmail.com>
2022-07-05 21:40:06 +0600

    all routes set up done

--- meal-db-prc/src/App.js
+++ meal-db-prc/src/App.js
@@ -1,8 +1,20 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Routes } from "react-router-dom";
+import Header from "./Components/Header/Header";
+import Home from "./Components/Home/Home";
+import Meals from "./Components/Meals/Meals";
+import Contact from "./Components/Contact/Contact";
+
 function App() {
   return (
     <div>
-      <h1>Meal Db</h1>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/meals" element={<Meals />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+      </Routes>
     </div>
   );
 }
--- meal-db-prc/src/Components/Header/Header.js
+++ meal-db-prc/src/Components/Header/Header.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
 
 const Header = () => {
-      return (
-            <div>
-                  
-            </div>
-      );
+  return (
+    <div>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <Nav.Link href="#action1">Home</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
 };
 
 export default Header;
