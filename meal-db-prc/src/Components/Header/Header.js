4e566b7d5e7a723950a589064d1ce163a640bf21
Salman-S-Rahman <salmanmern@gmail.com>
2022-07-05 21:56:48 +0600

    header part end

--- meal-db-prc/src/Components/Header/Header.js
+++ meal-db-prc/src/Components/Header/Header.js
@@ -1,20 +1,27 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Style from "./Header.module.css";
+import logo from "../../images/logo.png";
 
 const Header = () => {
   return (
     <div>
-      <Navbar bg="light" expand="lg">
+      <Navbar bg="dark" expand="lg">
         <Container>
-          <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+          <Navbar.Brand as={Link} to="/home">
+            <img src={logo} alt="" />
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
             <Nav
-              className="me-auto my-2 my-lg-0"
+              className={`${Style.Nav__container} ms-auto my-2 my-lg-0`}
               style={{ maxHeight: "100px" }}
               navbarScroll
             >
-              <Nav.Link href="#action1">Home</Nav.Link>
+              <Link to="/home">Home</Link>
+              <Link to="/meals">Meals</Link>
+              <Link to="/contact">Contact</Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
