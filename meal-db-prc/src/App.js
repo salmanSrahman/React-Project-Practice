a5227f330639879d2a62e4fb13bc6b2c15cd96ff
Salman-S-Rahman <salmanmern@gmail.com>
2022-07-06 02:57:25 +0600

    use navigate to see meal details

--- meal-db-prc/src/App.js
+++ meal-db-prc/src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Header from "./Components/Header/Header";
 import Home from "./Components/Home/Home";
 import Meals from "./Components/Meals/Meals";
+import MealDetails from "./Components/MealDetails/MealDetails";
 import Contact from "./Components/Contact/Contact";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/home" element={<Home />}></Route>
         <Route path="/meals" element={<Meals />}></Route>
+        <Route path="meal/:idMeal" element={<MealDetails />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
       </Routes>
     </div>
--- meal-db-prc/src/Components/Meal/Meal.js
+++ meal-db-prc/src/Components/Meal/Meal.js
@@ -1,15 +1,24 @@
 import React from "react";
 import { Button, Col } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 import Style from "./Meal.module.css";
 
 const Meal = ({ meal }) => {
-  const { strMeal, strMealThumb } = meal;
+  const { strMeal, strMealThumb, idMeal } = meal;
+
+  const navigate = useNavigate();
+
   return (
     <Col>
       <div className={Style.Meal__container}>
         <img src={strMealThumb} className="img-fluid" alt="" />
         <h4 className="text-center py-2">{strMeal}</h4>
-        <Button className="d-block w-100">See Details</Button>
+        <Button
+          className="d-block w-100"
+          onClick={() => navigate(`/meal/${idMeal}`)}
+        >
+          See Details
+        </Button>
       </div>
     </Col>
   );
