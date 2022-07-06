26580b6334304dd2648758641b52100a8028dc71
Salman-S-Rahman <salmanmern@gmail.com>
2022-07-06 21:50:47 +0600

    local storage db created

--- ulupono-prc/src/utilities/fakeDb.js
+++ ulupono-prc/src/utilities/fakeDb.js
@@ -18,4 +18,28 @@ const addToDb = (id) => {
   localStorage.setItem("shopping-Cart", JSON.stringify(shoppingCart));
 };
 
-export { addToDb };
+const removeFromDb = (id) => {
+  const storedCart = localStorage.getItem("shopping-Cart");
+  if (storedCart) {
+    const shoppingCart = JSON.parse(storedCart);
+    if (id in shoppingCart) {
+      delete shoppingCart[id];
+    }
+    localStorage.setItem("shopping-cart", JSON.stringify(shoppingCart));
+  }
+};
+
+const deleteShoppingCart = () => {
+  localStorage.removeItem("shopping-Cart");
+};
+
+const getStoredCart = () => {
+  let shoppingCart = {};
+  const storedCart = localStorage.getItem("shopping-cart");
+  if (storedCart) {
+    shoppingCart = JSON.parse(storedCart);
+  }
+  return shoppingCart;
+};
+
+export { addToDb, removeFromDb, deleteShoppingCart, getStoredCart };
