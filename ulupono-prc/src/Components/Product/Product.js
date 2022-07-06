5af48f9220f68a3f2ca8667202cd8a561c176bf6
Salman-S-Rahman <salmanmern@gmail.com>
2022-07-06 16:04:44 +0600

    fetch data & products shown in display

--- ulupono-prc/src/Components/Product/Product.js
+++ ulupono-prc/src/Components/Product/Product.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Row, Col } from "react-bootstrap";
 import Style from "./Product.module.css";
+import { BsCartPlusFill } from "react-icons/bs";
+import Rating from "react-rating";
 
 const Product = ({ product }) => {
   console.log(product);
@@ -8,23 +10,39 @@ const Product = ({ product }) => {
     product;
 
   return (
-    <Row className="g-3">
+    <Row className={`${Style.product__details} g-3`}>
       <Col xl={3}>
         <img src={img} className="img-fluid" alt="" />
       </Col>
       <Col xl={9}>
-        <h5>{name}</h5>
-        <Row>
-          <Col xl={9}>
+        <h5 style={{ color: "blue" }}>{name}</h5>
+        <Row className="py-3">
+          <Col xl={6}>
             <div>
               <h6>By:{seller}</h6>
               <h6>Price: {price}</h6>
               <h6>only {} left in stock - order soon</h6>
-              <button className={Style.btn__regular}>Add To Cart</button>
+              <div>
+                <Rating
+                  initialRating={star}
+                  emptySymbol="fa fa-star-o fa-2x"
+                  fullSymbol="fa fa-star fa-2x"
+                  readonly
+                  className={Style.rating__part}
+                />
+                <span className="text-secondary"> ({starCount})</span>
+              </div>
+              <button className={`${Style.btn__regular} mt-3`}>
+                Add To Cart <BsCartPlusFill className="fs-5" />
+              </button>
             </div>
           </Col>
-          <Col xl={3}>
-            <h4>Features:</h4>
+          <Col xl={6}>
+            <div>
+              <div>
+                <h6>Features:</h6>
+              </div>
+            </div>
           </Col>
         </Row>
       </Col>
--- ulupono-prc/src/Components/Shop/Shop.js
+++ ulupono-prc/src/Components/Shop/Shop.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Product from "../Product/Product";
+import Style from './Shop.module.css'
 
 const Shop = () => {
   const [products, setProducts] = useState([]);
@@ -15,13 +16,15 @@ const Shop = () => {
       <Container>
         <Row className="g-3">
           <Col xl={9}>
-            <div>
+            <div className={Style.product__container}>
               {products.map((product) => (
                 <Product key={product.key} product={product}></Product>
               ))}
             </div>
           </Col>
-          <Col xl={3}></Col>
+          <Col xl={3}>
+            <h1>Cart</h1>
+          </Col>
         </Row>
       </Container>
     </div>
