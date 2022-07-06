1571e6065c917e1523bb64c81c49424200f56b82
Salman-S-Rahman <salmanmern@gmail.com>
2022-07-07 04:01:52 +0600

    order detyails shown

--- ulupono-prc/src/Components/Order/Order.js
+++ ulupono-prc/src/Components/Order/Order.js
@@ -3,9 +3,11 @@ import { Container, Row, Col } from "react-bootstrap";
 import { useCart } from "../../Hooks/useCart";
 import { useProducts } from "../../Hooks/useProducts";
 import Cart from "../Cart/Cart";
+import Style from "./Order.module.css";
+import { RiDeleteBin6Line } from "react-icons/ri";
 
 const Order = () => {
-  const [products, setProducts] = useProducts();
+  const [products] = useProducts();
   const [cart] = useCart(products);
 
   return (
@@ -14,24 +16,27 @@ const Order = () => {
         <Row>
           <Col xl={9}>
             <div>
-              <Row>
+              <Row className="g-3">
                 <Col xl={8}>
                   {cart.map((product) => (
-                    <div>
-                      <Row>
-                        <Col xl={4}>
+                    <div className={`${Style.orderProduct__container}`}>
+                      <Row className="g-3">
+                        <Col xl={3}>
                           <img src={product.img} className="img-fluid" alt="" />
                         </Col>
-                        <Col xl={8}>
-                          <div className="d-flex justify-content-between">
+                        <Col
+                          xl={9}
+                          className="d-flex justify-content-between align-items-center"
+                        >
+                          <div>
                             <div>
                               <h6>{product.name}</h6>
                               <h6>Price: ${product.price}</h6>
                               <h6>Shipping Charge: ${product.shipping}</h6>
                             </div>
-                            <div>
-                              <h3>Delete</h3>
-                            </div>
+                          </div>
+                          <div>
+                            <RiDeleteBin6Line className={Style.delete__btn} />
                           </div>
                         </Col>
                       </Row>
