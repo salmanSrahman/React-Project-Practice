4d6a40455599a22907960681dde126bab051b64a
Salman-S-Rahman <salmanmern@gmail.com>
2022-07-06 12:09:01 +0600

    header part end & custom link used

--- ulupono-prc/src/App.js
+++ ulupono-prc/src/App.js
@@ -1,9 +1,10 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <div>
-      <h1>Ulupono E-commerce</h1>
+      <Header />
     </div>
   );
 }
--- ulupono-prc/src/Components/Header/Header.js
+++ ulupono-prc/src/Components/Header/Header.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import Style from './Header.module.css'
+import React from "react";
+import Style from "./Header.module.css";
+import Navbar from "react-bootstrap/Navbar";
+import { Container, Nav } from "react-bootstrap";
+import logo from "../../images/logo.png";
+import CustomLink from "../CustomLink/CustomLink";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-      return (
-            <div>
-                  
-            </div>
-      );
+  return (
+    <div>
+      <Navbar bg="dark" expand="lg">
+        <Container>
+          <Navbar.Brand as={Link} to="/shop">
+            <img src={logo} className="w-50" alt="" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className={`${Style.nav__container} ms-auto my-2 my-lg-0`}
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <CustomLink to="/shop">Shop</CustomLink>
+              <CustomLink to="/order">Order Review</CustomLink>
+              <CustomLink to="/inventory">Manage Inventory</CustomLink>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
 };
 
 export default Header;
