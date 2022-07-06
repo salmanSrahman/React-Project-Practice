b292bc70ff2088c45b96186790cff137aa210a2c
Salman-S-Rahman <salmanmern@gmail.com>
2022-07-06 13:42:02 +0600

    fetch data & products shown in display

--- ulupono-prc/src/Components/Product/Product.js
+++ ulupono-prc/src/Components/Product/Product.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import Style from "./Product.module.css";
 
-const Product = () => {
-      return (
+const Product = ({ product }) => {
+  console.log(product);
+  const { name, price, img, seller, star, starCount, stock, features } =
+    product;
+
+  return (
+    <Row className="g-3">
+      <Col xl={3}>
+        <img src={img} className="img-fluid" alt="" />
+      </Col>
+      <Col xl={9}>
+        <h5>{name}</h5>
+        <Row>
+          <Col xl={9}>
             <div>
-                  
+              <h6>By:{seller}</h6>
+              <h6>Price: {price}</h6>
+              <h6>only {} left in stock - order soon</h6>
+              <button className={Style.btn__regular}>Add To Cart</button>
             </div>
-      );
+          </Col>
+          <Col xl={3}>
+            <h4>Features:</h4>
+          </Col>
+        </Row>
+      </Col>
+    </Row>
+  );
 };
 
 export default Product;
--- ulupono-prc/src/Components/Shop/Shop.js
+++ ulupono-prc/src/Components/Shop/Shop.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import Product from "../Product/Product";
 
 const Shop = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
   return (
     <div>
-      <h1>Shop</h1>
+      <Container>
+        <Row className="g-3">
+          <Col xl={9}>
+            <div>
+              {products.map((product) => (
+                <Product key={product.key} product={product}></Product>
+              ))}
+            </div>
+          </Col>
+          <Col xl={3}></Col>
+        </Row>
+      </Container>
     </div>
   );
 };
