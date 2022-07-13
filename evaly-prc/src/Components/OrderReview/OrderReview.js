adbff47cc8a9c508496b6195ef081d58b4fa244d
Salman-S-Rahman <salmanmern@gmail.com>
2022-07-13 20:53:41 +0600

    all components created

---
+++ evaly-prc/src/Components/OrderReview/OrderReview.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './OrderReview.css'
+
+const OrderReview = () => {
+      return (
+            <div>
+                  
+            </div>
+      );
+};
+
+export default OrderReview;
