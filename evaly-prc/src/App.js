c5941a38560b4f89bd4d58e7949f68978bf2054f
Salman-S-Rahman <salmanmern@gmail.com>
2022-07-13 21:25:40 +0600

    all routes set up done

--- evaly-prc/src/App.js
+++ evaly-prc/src/App.js
@@ -1,9 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes, Route } from "react-router-dom";
 import Header from "./Components/Header/Header";
+import ManageInventory from "./Components/ManageInventory/ManageInventory";
+import OrderReview from "./Components/OrderReview/OrderReview";
+import Shop from "./Components/Shop/Shop";
 function App() {
   return (
     <div>
       <Header />
+      <Routes>
+        <Route path="/" element={<Shop />}></Route>
+        <Route path="shop" element={<Shop />}></Route>
+        <Route path="orderReview" element={<OrderReview />}></Route>
+        <Route path="manageInventory" element={<ManageInventory />}></Route>
+      </Routes>
     </div>
   );
 }
