b2113d60952a17380d4979548e75c435f5364885
Salman-S-Rahman <salmanmern@gmail.com>
2022-07-14 05:21:10 -0500

    featch data & all products information shown in ui

--- evaly-prc/src/Components/Product/Product.js
+++ evaly-prc/src/Components/Product/Product.js
@@ -1,12 +1,51 @@
-import React from 'react';
-import './Product.css'
+import React from "react";
+import "./Product.css";
+import { Row, Col } from "react-bootstrap";
+import "./Product.css";
+import Rating from "react-rating";
 
-const Product = () => {
-      return (
+const Product = ({ product }) => {
+  console.log(product.features);
+  const { name, price, img, stock, star, starCount, seller, features } =
+    product;
+  return (
+    <div className="product__details">
+      <Row className="g-3">
+        <Col xl={3}>
+          <img src={img} className="img-fluid" alt="" />
+        </Col>
+        <Col xl={9}>
+          <h6>{name}</h6>
+          <div className="d-flex py-2">
             <div>
-                  
+              <h6>By: {seller}</h6>
+              <h6>Price: ${price}</h6>
+              <h6>
+                only <span className="text-danger fw-bold">{stock}</span> left
+                in stock -
+                <span className="font__color fw-bold"> Order Soon</span>
+              </h6>
+              <div>
+                <Rating
+                  initialRating={star}
+                  emptySymbol="fa fa-star-o fa-2x"
+                  fullSymbol="fa fa-star fa-2x"
+                  readonly
+                  className="ratings pb-3"
+                />
+                <span className="text-secondary fw-bold">({starCount} )</span>
+              </div>
+              <button className="btn__regular">Add To Cart</button>
             </div>
-      );
+            <div className="ps-4">
+              <h5>Features:</h5>
+              
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
 export default Product;
--- evaly-prc/src/Components/Shop/Shop.js
+++ evaly-prc/src/Components/Shop/Shop.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import './Shop.css'
+import React, { useEffect, useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import Product from "../Product/Product";
+import "./Shop.css";
 
 const Shop = () => {
-      return (
-            <div>
-                  
-            </div>
-      );
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+  return (
+    <div>
+      <Container>
+        <Row className="g-3">
+          <Col xl={9}>
+            {products.map((product) => (
+              <Product key={product.key} product={product}></Product>
+            ))}
+          </Col>
+          <Col xl={3}>
+            <h3>Cart</h3>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default Shop;
