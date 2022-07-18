10b28ca6ac076440f89d9f0f7f687a73f909f335
Salman-S-Rahman <salmanmern@gmail.com>
2022-07-18 21:47:28 +0600

    banner part end

--- creative-acd/src/Components/Home/Banner/Banner.js
+++ creative-acd/src/Components/Home/Banner/Banner.js
@@ -1,12 +1,97 @@
-import React from 'react';
-import './Banner.css'
+import React from "react";
+import "./Banner.css";
+import Carousel from "react-bootstrap/Carousel";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import slider1 from "../../../images/slider1.png";
 
 const Banner = () => {
-      return (
-            <div>
-                  <h1>Banner</h1>
-            </div>
-      );
+  return (
+    <div>
+      <Carousel variant="dark" className="banner__container">
+        <Carousel.Item>
+          <Container>
+            <Row className="g-3">
+              <Col xl={7} className="d-flex align-items-center">
+                <div className="banner__text">
+                  <h1>
+                    Development
+                    <span className="d-block">Is Our Goal</span>
+                    <span className="d-block">Ever</span>
+                  </h1>
+                  <p className="py-2">
+                    We are a boutique digital transformation consultancy and
+                    software development company.
+                  </p>
+                  <Button size="lg" variant="danger">
+                    Start Free Trial
+                  </Button>
+                </div>
+              </Col>
+              <Col xl={5}>
+                <div className="banner__img">
+                  <img src={slider1} className="img-fluid" alt="" />
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Container>
+            <Row className="g-3">
+              <Col xl={7} className="d-flex align-items-center">
+                <div className="banner__text">
+                  <h1>
+                    Development
+                    <span className="d-block">Is Our Goal</span>
+                    <span className="d-block">Ever</span>
+                  </h1>
+                  <p className="py-2">
+                    We are a boutique digital transformation consultancy and
+                    software development company.
+                  </p>
+                  <Button size="lg" variant="danger">
+                    Start Free Trial
+                  </Button>
+                </div>
+              </Col>
+              <Col xl={5}>
+                <div className="banner__img">
+                  <img src={slider1} className="img-fluid" alt="" />
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Container>
+            <Row className="g-3">
+              <Col xl={7} className="d-flex align-items-center">
+                <div className="banner__text">
+                  <h1>
+                    Development
+                    <span className="d-block">Is Our Goal</span>
+                    <span className="d-block">Ever</span>
+                  </h1>
+                  <p className="py-2">
+                    We are a boutique digital transformation consultancy and
+                    software development company.
+                  </p>
+                  <Button size="lg" variant="danger">
+                    Start Free Trial
+                  </Button>
+                </div>
+              </Col>
+              <Col xl={5}>
+                <div className="banner__img">
+                  <img src={slider1} className="img-fluid" alt="" />
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
 };
 
 export default Banner;
