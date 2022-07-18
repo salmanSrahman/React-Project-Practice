539190b6fbcd64b1bb38e07f2f9e7369a40e0224
Salman-S-Rahman <salmanmern@gmail.com>
2022-07-18 13:56:46 +0600

    all components created

---
+++ creative-acd/src/Components/Home/HomeServices/HomeServices.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './HomeServices.css'
+
+const HomeServices = () => {
+      return (
+            <div>
+                  
+            </div>
+      );
+};
+
+export default HomeServices;
