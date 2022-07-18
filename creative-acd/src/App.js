72419cf9a4ee70e873b999da181e40362c148e83
Salman-S-Rahman <salmanmern@gmail.com>
2022-07-18 13:24:06 +0600

    creative academy website

---
+++ creative-acd/src/App.js
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Creative academy project</h1>
+    </div>
+  );
+}
+
+export default App;
