29ed68fd49f9f966bacbcb4699e4cc778513c6a2
Salman-S-Rahman <salmanmern@gmail.com>
2022-07-18 16:31:05 +0600

    all rotes set up done

--- creative-acd/src/App.js
+++ creative-acd/src/App.js
@@ -1,11 +1,25 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Header from "./Components/Shared/Header/Header";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Components/Home/Home";
+import About from "./Components/About/About";
+import Services from "./Components/Services/Services";
+import Faq from "./Components/FAQ/Faq";
+import Contact from "./Components/Contact/Contact";
 
 function App() {
   return (
     <div>
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="home" element={<Home />}></Route>
+        <Route path="about" element={<About />}></Route>
+        <Route path="services" element={<Services />}></Route>
+        <Route path="faq" element={<Faq />}></Route>
+        <Route path="contact" element={<Contact />}></Route>
+      </Routes>
     </div>
   );
 }
