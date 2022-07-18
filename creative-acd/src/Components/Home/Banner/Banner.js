391a2a6b900e3e41b27dbad3ee265e50a265d6cf
Salman-S-Rahman <salmanmern@gmail.com>
2022-07-18 22:00:23 +0600

    banner part end

--- creative-acd/src/Components/Home/Banner/Banner.js
+++ creative-acd/src/Components/Home/Banner/Banner.js
@@ -3,6 +3,8 @@ import "./Banner.css";
 import Carousel from "react-bootstrap/Carousel";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import slider1 from "../../../images/slider1.png";
+import slider2 from "../../../images/slider2.png";
+import slider3 from "../../../images/slider3.png";
 
 const Banner = () => {
   return (
@@ -38,7 +40,7 @@ const Banner = () => {
         <Carousel.Item>
           <Container>
             <Row className="g-3">
-              <Col xl={7} className="d-flex align-items-center">
+              <Col xl={8} className="d-flex align-items-center">
                 <div className="banner__text">
                   <h1>
                     Development
@@ -54,9 +56,9 @@ const Banner = () => {
                   </Button>
                 </div>
               </Col>
-              <Col xl={5}>
+              <Col xl={4}>
                 <div className="banner__img">
-                  <img src={slider1} className="img-fluid" alt="" />
+                  <img src={slider2} className="img-fluid" alt="" />
                 </div>
               </Col>
             </Row>
@@ -83,7 +85,7 @@ const Banner = () => {
               </Col>
               <Col xl={5}>
                 <div className="banner__img">
-                  <img src={slider1} className="img-fluid" alt="" />
+                  <img src={slider3} className="img-fluid" alt="" />
                 </div>
               </Col>
             </Row>
