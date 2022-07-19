151bc68829f630606b3b5ffc3eb6f442839ceffc
Salman-S-Rahman <salmanmern@gmail.com>
2022-07-19 13:00:46 +0600

    title part end

--- creative-acd/src/Components/Home/Home.js
+++ creative-acd/src/Components/Home/Home.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import Banner from './Banner/Banner';
+import React from "react";
+import Banner from "./Banner/Banner";
+import HomeServices from "./HomeServices/HomeServices";
 
 const Home = () => {
-      return (
-            <div>
-                  <Banner/>
-            </div>
-      );
+  return (
+    <div>
+      <Banner />
+      <HomeServices />
+    </div>
+  );
 };
 
 export default Home;
--- creative-acd/src/Components/Home/HomeServices/HomeServices.js
+++ creative-acd/src/Components/Home/HomeServices/HomeServices.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import './HomeServices.css'
+import React from "react";
+import { Container } from "react-bootstrap";
+import "./HomeServices.css";
 
 const HomeServices = () => {
-      return (
-            <div>
-                  
-            </div>
-      );
+  return (
+    <div className="homeService__part">
+      <Container>
+        <div>
+          <div className="title_container d-flex justify-content-center">
+          <h2 className="title text-uppercase">Our Best Services</h2>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default HomeServices;
