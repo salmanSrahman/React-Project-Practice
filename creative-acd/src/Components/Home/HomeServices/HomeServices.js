25eccc5003333fa3311d31c4b8c555502faaaae4
Salman-S-Rahman <salmanmern@gmail.com>
2022-07-19 16:27:31 +0600

    home service details shown

--- creative-acd/src/Components/Home/HomeServices/HomeServices.js
+++ creative-acd/src/Components/Home/HomeServices/HomeServices.js
@@ -1,15 +1,23 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import "./HomeServices.css";
+import services from "../../../images/program.gif";
 
 const HomeServices = () => {
+
   return (
     <div className="homeService__part">
       <Container>
         <div>
           <div className="title_container d-flex justify-content-center">
-          <h2 className="title text-uppercase">Our Best Services</h2>
+            <h2 className="title text-uppercase">Our Best Services</h2>
           </div>
+          <Row>
+            <Col xl={5}>
+              <img src={services} className="img-fluid" alt="" />
+            </Col>
+            <Col xl={7}></Col>
+          </Row>
         </div>
       </Container>
     </div>
