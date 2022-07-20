0d159120a43d8fbe94bc22c46c1e8c9038447c60
Salman-S-Rahman <salmanmern@gmail.com>
2022-07-20 21:37:20 +0600

    get started button added

--- creative-acd/src/Components/Contact/Contact.js
+++ creative-acd/src/Components/Contact/Contact.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import './Contact.css'
+import React from "react";
+import "./Contact.css";
 
 const Contact = () => {
-      return (
-            <div>
-                  
-            </div>
-      );
+  return (
+    <div>
+      <h1>Contact part Is Not Added Here.</h1>
+    </div>
+  );
 };
 
 export default Contact;
